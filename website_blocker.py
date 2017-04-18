c9aeb191804eca43d6528b3a76ab538fb04554c4
fadnavis <harsh.fad@gmail.com>
2017-04-18 20:20:44 +0200

    Add website blocker code

---
+++ website_blocker.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime as d
+
+localhost = "127.0.0.1"
+websitelist = ["www.facebook.com","www.twitter.com","facebook.com","twitter.com"]
+host_path = "/etc/hosts"
+host_temp = "hosts"
+
+while True:
+    if d(d.now().year,d.now().month,d.now().day,8) < d.now() < d(d.now().year,d.now().month, d.now().day,18):
+        print("Working time..")
+        with open(host_temp,'r+') as file:
+            content = file.read()
+            for website in websitelist:
+                if website in content:
+                    pass
+                else:
+                    file.write(localhost + " " + website + "\n")
+    else:
+        print("Personal time..")
+        with open(host_temp,'r+') as file:
+            contentlist = file.readlines()
+            file.seek(0)
+            for line in contentlist:
+                if all(website not in line for website in websitelist):
+                    file.write(line)
+            file.truncate()
+    time.sleep(5)
