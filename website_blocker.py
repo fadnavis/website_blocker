3c1ef06f0ab379535f732556bc2baf3fd8cbea54
fadnavis <harsh.fad@gmail.com>
2017-04-18 23:11:33 +0200

    Remove host_temp variable

--- website_blocker.py
+++ website_blocker.py
@@ -4,12 +4,11 @@ from datetime import datetime as d
 localhost = "127.0.0.1"
 websitelist = ["www.facebook.com","www.twitter.com","facebook.com","twitter.com"]
 host_path = "/etc/hosts"
-host_temp = "hosts"
 
 while True:
     if d(d.now().year,d.now().month,d.now().day,9) < d.now() < d(d.now().year,d.now().month, d.now().day,18):
         print("Working time..")
-        with open(host_temp,'r+') as file:
+        with open(host_path,'r+') as file:
             content = file.read()
             for website in websitelist:
                 if website in content:
@@ -18,7 +17,7 @@ while True:
                     file.write(localhost + " " + website + "\n")
     else:
         print("Personal time..")
-        with open(host_temp,'r+') as file:
+        with open(host_path,'r+') as file:
             contentlist = file.readlines()
             file.seek(0)
             for line in contentlist:
